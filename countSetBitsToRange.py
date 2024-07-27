b5bc163eb8317884842136da0639f1379f258095
mohits44 <101943239+mohits44@users.noreply.github.com>
2024-07-28 02:23:26 +0530

    Update countSetBitsToRange.py

--- countSetBitsToRange.py
+++ countSetBitsToRange.py
@@ -1,1 +1,25 @@
+class Solution:
+    #Function to return sum of count of set bits in the integers from 1 to n.
+    def countSetBits(self,n):
+        # code here
+        # return the count
+        count=0
+        for i in range(1,n+1):
+            while i!=0:
+                if i&1==1:
+                    count=count+1
+                i=i>>1
+            count=count+i
+        return count
 
+
+# basic version
+# def countSetBits(n):
+#     count=0
+#     while n!=0:
+#         if n&1==1:
+#             count=count+1
+#         n=n>>1
+#     return count
+
+# print(countSetBits(5))
