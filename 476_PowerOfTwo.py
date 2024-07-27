0c5c008b96eef631e263da34dc3ab6619102ce1f
mohits44 <101943239+mohits44@users.noreply.github.com>
2024-07-28 02:07:41 +0530

    Update 476_PowerOfTwo.py

--- 476_PowerOfTwo.py
+++ 476_PowerOfTwo.py
@@ -1,1 +1,12 @@
+def powerOfTwo(n):    
+    i=1
+    while i<=n:
+        if i==n:
+            return True
+        i=i*2
+    return False
 
+if (powerOfTwo(15)):
+    print("True")
+else:
+    print("false")
